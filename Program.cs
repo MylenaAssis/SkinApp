59ba3519cd403ed42ba2f883e57933532ba7eaf4
Mylena Assis <lena.assis.dev@gmail.com>
2024-05-25 18:20:04 -0300

    Added: logo e menu inicial

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+void ExibirLogo ()
+{
+    Console.WriteLine(@"
+
+██████████████████████████████████████████████████
+█─▄▄▄▄█▄─█─▄█▄─▄█▄─▀█▄─▄█▀▀▀▀▀███▀▄─██▄─▄▄─█▄─▄▄─█
+█▄▄▄▄─██─▄▀███─███─█▄▀─██████████─▀─███─▄▄▄██─▄▄▄█
+▀▄▄▄▄▄▀▄▄▀▄▄▀▄▄▄▀▄▄▄▀▀▄▄▀▀▀▀▀▀▀▀▄▄▀▄▄▀▄▄▄▀▀▀▄▄▄▀▀▀
+    ");
+    Console.WriteLine("O seu aplicativo de dicas de SkinCare.");
+}
+
+void ExibirMenu ()
+{
+    Console.WriteLine("--------------------------------------------------");
+    Console.WriteLine("\n\nMENU INICIAL");
+    Console.WriteLine("1. Começar informando suas queixas.");
+    Console.WriteLine("2. Buscar produto pela função.");
+    Console.WriteLine("3. Buscar produtos recomendados por marca.");
+    Console.WriteLine("4. Buscar produtos por faixa de preço.");
+    Console.WriteLine("5. Buscar produtos por média de avaliação.");
+    Console.WriteLine("\nDigite a opção escolhida: ");
+    string opcaoEscolhida = Console.ReadLine()!;
+}
+
+ExibirLogo();
+ExibirMenu();
